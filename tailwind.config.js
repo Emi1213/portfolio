af2f087f34caca4877309fb6176618a0f6425f93
Emi1213 <emi1213galarza@gmail.com>
2024-01-30 01:18:47 -0500

    NavBar done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        haiti: {
+          50: "#f2eeff",
+          100: "#e7e0ff",
+          200: "#d4c6ff",
+          300: "#bda4fd",
+          400: "#ab80f9",
+          500: "#a061f3",
+          600: "#9644e7",
+          700: "#8636cc",
+          800: "#6b2fa4",
+          900: "#562d82",
+          950: "#29153c",
+        },
+      },
+      spacing: {
+        112: "28rem",
+        128: "32rem",
+        144: "36rem",
+        140: "40rem",
+      },
+    },
   },
   plugins: [],
 };
